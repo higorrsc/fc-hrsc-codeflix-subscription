529990ea2e33f1687248bc2fff6188c1d576b6fb
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-08 17:54:44 -0300

    test: :white_check_mark: add user account tests

---
+++ src/test_user_account.py
@@ -0,0 +1,18 @@
+from src.user_account import Address, UserAccount
+
+
+def test_create_valid_user_account():
+    user_account = UserAccount(
+        iam_user_id="1234567890123",
+        name="John Doe",
+        email="john.doe@examplepetstore.com",
+        billing_address=Address(
+            street="123 Main St",
+            city="Anytown",
+            state="CA",
+            zip_code="12345",
+            country="USA",
+        ),
+    )
+
+    assert user_account.id is not None
