d7bac895e7f08d756beb623c1589b9a4bc740dc9
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-08 20:39:05 -0300

    feat: ✨ add duplicated user exception

--- src/application/exceptions.py
+++ src/application/exceptions.py
@@ -2,3 +2,9 @@ class DuplicatePlanError(Exception):
     """
     Exception raised when trying to create a plan that already exists.
     """
+
+
+class UserAlreadyExistsError(Exception):
+    """
+    Exception raised when trying to create a user that already exists.
+    """
