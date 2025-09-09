03cef9ccd463dbf671521aef211355039c9969b2
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-08 21:28:12 -0300

    feat: ✨ add cancel subscription use case

---
+++ src/application/cancel_subscription.py
@@ -0,0 +1,37 @@
+from uuid import UUID
+
+from pydantic import BaseModel
+
+from src.application.exceptions import SubscriptionNotFoundError
+
+
+class CancelSubscriptionInputDTO(BaseModel):
+    """
+    Input DTO for canceling a subscription.
+    """
+
+    id: UUID
+
+
+class CancelSubscriptionUseCase:
+    """
+    Use case for canceling a subscription.
+    """
+
+    def __init__(self, repository) -> None:
+        """
+        Initialize the use case.
+        """
+
+        self._repository = repository
+
+    def execute(self, input_dto: CancelSubscriptionInputDTO) -> None:
+        """
+        Execute the use case.
+        """
+
+        subscription = self._repository.get_by_id(input_dto.id)
+        if not subscription:
+            raise SubscriptionNotFoundError("Subscription not found")
+
+        subscription.cancel()
