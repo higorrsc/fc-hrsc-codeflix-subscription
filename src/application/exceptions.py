fea4f216f8ae75e9a02e5c0e2802028465c03a53
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-08 21:27:06 -0300

    feat: ✨ add subscription exceptions

--- src/application/exceptions.py
+++ src/application/exceptions.py
@@ -8,3 +8,15 @@ class UserAlreadyExistsError(Exception):
     """
     Exception raised when trying to create a user that already exists.
     """
+
+
+class SubscriptionNotFoundError(Exception):
+    """
+    Exception raised when trying to cancel a subscription that does not exist.
+    """
+
+
+class SubscriptionAlreadyExistsError(Exception):
+    """
+    Exception raised when trying to create a subscription that already exists.
+    """
