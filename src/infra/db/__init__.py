a48828b996993e44958942d9680f912fdeb92d8f
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-11 20:48:27 -0300

    feat: ✨ add database initiation

---
+++ src/infra/db/__init__.py
@@ -0,0 +1,30 @@
+from sqlmodel import Session, SQLModel, create_engine
+
+from .repository import SQLModelPlanRepository
+
+DATABASE_URL = "sqlite:///./subscription_service.db"
+engine = create_engine(DATABASE_URL)
+
+
+def create_db_and_tables() -> None:
+    """
+    Create database and tables.
+    """
+
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    """
+    Returns a database session.
+    """
+
+    with Session(engine) as session:
+        yield session
+
+
+__all__ = [
+    "SQLModelPlanRepository",
+    "create_db_and_tables",
+    "get_session",
+]
