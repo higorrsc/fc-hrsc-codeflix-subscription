fc3a2b0be352f0f128280f76dbafa53e5a334ee0
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-12 20:01:30 -0300

    feat: ✨ add FastAPI application configuration

---
+++ src/infra/api/app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.concurrency import asynccontextmanager
+
+from src.infra.api.routes import PlansRouter
+from src.infra.db import create_db_and_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Startup event.
+    """
+
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(title="Subscription Service API", lifespan=lifespan)
+
+app.include_router(PlansRouter)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
