9f106e8562d97ac47dd63d10217db5c2bc158bcc
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-12 21:43:29 -0300

    refactor: ♻ add all classes to infra/db/repository init file

--- src/infra/db/repository/__init__.py
+++ src/infra/db/repository/__init__.py
@@ -1,3 +1,9 @@
 from .sql_model_plan_repository import SQLModelPlanRepository
-from .sql_model_user_account_repository import SQLModelUserAccountRepository
 from .sql_model_subscription_repository import SQLModelSubscriptionRepository
+from .sql_model_user_account_repository import SQLModelUserAccountRepository
+
+__all__ = [
+    "SQLModelPlanRepository",
+    "SQLModelUserAccountRepository",
+    "SQLModelSubscriptionRepository",
+]
