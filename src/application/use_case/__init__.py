45dbd0a82e09bcd1e68bfde4500e43f053fd5db6
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-12 21:41:48 -0300

    refactor: ♻ add all classes to use case init file

--- src/application/use_case/__init__.py
+++ src/application/use_case/__init__.py
@@ -1,5 +1,34 @@
 from .cancel_subscription import CancelSubscriptionInputDTO, CancelSubscriptionUseCase
 from .create_plan import CreatePlanInputDTO, CreatePlanOutputDTO, CreatePlanUseCase
-from .create_user_account import CreateUserAccountInputDTO, CreateUserAccountOutputDTO, CreateUserAccountUseCase
-from .renew_subscription import RenewSubscriptionInputDTO, RenewSubscriptionOutputDTO, RenewSubscriptionUseCase
-from .subscribe_to_plan import SubscribeToPlanInputDTO, SubscribeToPlanOutputDTO, SubscribeToPlanUseCase
+from .create_user_account import (
+    CreateUserAccountInputDTO,
+    CreateUserAccountOutputDTO,
+    CreateUserAccountUseCase,
+)
+from .renew_subscription import (
+    RenewSubscriptionInputDTO,
+    RenewSubscriptionOutputDTO,
+    RenewSubscriptionUseCase,
+)
+from .subscribe_to_plan import (
+    SubscribeToPlanInputDTO,
+    SubscribeToPlanOutputDTO,
+    SubscribeToPlanUseCase,
+)
+
+__all__ = [
+    "CancelSubscriptionInputDTO",
+    "CancelSubscriptionUseCase",
+    "CreatePlanInputDTO",
+    "CreatePlanOutputDTO",
+    "CreatePlanUseCase",
+    "CreateUserAccountInputDTO",
+    "CreateUserAccountOutputDTO",
+    "CreateUserAccountUseCase",
+    "RenewSubscriptionInputDTO",
+    "RenewSubscriptionOutputDTO",
+    "RenewSubscriptionUseCase",
+    "SubscribeToPlanInputDTO",
+    "SubscribeToPlanOutputDTO",
+    "SubscribeToPlanUseCase",
+]
